af042ee34e731b5c94c297669d5787d7f349c751
KarlaJu <karla@makingdevs.com>
2019-05-25 19:48:27 -0500

    Adding integer, float and boolean data types

--- data_type.py
+++ data_type.py
@@ -1,10 +1,22 @@
+#Strings
 print("A string with double quotes")
 print('A string with simple quotes')
 print('''A string with
   three simple quotes''')
 print("""A string with
   three double quotes""")
-
-print(type("Return the data type"))
-
+print(type("Type function return the data type"))
 print("Two differents strings" + " united")
+
+#Integers
+print("A integer number")
+print(10)
+
+#Float
+print("A float number")
+print(7.3)
+
+#Boolean
+print("The boolean values")
+print(True)
+print(False)
