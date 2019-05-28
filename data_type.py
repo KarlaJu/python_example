c0a80a377e141ab6c08d0b128aa346bd00564dea
KarlaJu <karla@makingdevs.com>
2019-05-27 20:33:36 -0500

    A simple way to print

--- data_type.py
+++ data_type.py
@@ -9,17 +9,13 @@ print(type("Type function return the data type"))
 print("Two differents strings" + " united")
 
 #Integers
-print("A integer number")
-print(10)
+print("A integer number", 10)
 
 #Float
-print("A float number")
-print(7.3)
+print("A float number", 7.3)
 
 #Boolean
-print("The boolean values")
-print(True)
-print(False)
+print("The boolean values", True, False)
 
 #List
 print("The lists")
@@ -29,3 +25,10 @@ print([1, "Hello", 1.2, True])
 #Tuples
 print("Tuples")
 print(200,"Ok")
+
+#Dictionary
+print({
+  "name": "Someone",
+  "song": "I did",
+  "flower": "Sunflower"
+})
